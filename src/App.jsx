15e500e5845b320b2a7f37c991c6442f7d88c7f6
Gabr4ca <jvgamingvn@gmail.com>
2025-10-04 18:40:25 +0700

    something something

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Footer from './components/Footer/Footer'
-import LoginPopup from './components/LoginPopup/LoginPopup'
+import React, {useState} from "react";
+import Navbar from "./components/Navbar/Navbar";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
+
+// 1. Import the new GoToTopButton component
+import GoToTopButton from "./components/GoToTop/GoToTop";
 
 const App = () => {
-
-  const [showLogin, setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-      <div className='app'>
-        <Navbar setShowLogin={setShowLogin}/>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/order' element={<PlaceOrder />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
         </Routes>
       </div>
       <Footer />
-    </>
-  )
-}
 
-export default App
+      {/* 2. Add the GoToTopButton here */}
+      <GoToTopButton />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/GoToTop/GoToTop.jsx
@@ -0,0 +1,59 @@
+// src/components/GoToTopButton/GoToTopButton.jsx
+import React, {useState, useEffect} from "react";
+import "./GoToTop.css"; // We will create this CSS file next
+
+const GoToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled down
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Set up a listener for the scroll event
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    // Clean up the listener when the component is unmounted
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  // Scroll to top smoothly
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="go-to-top">
+      {isVisible && (
+        <button onClick={scrollToTop} className="top-btn" title="Go to top">
+          {/* You can use an SVG icon here for better visuals */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M12 19V5M5 12l7-7 7 7" />
+          </svg>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default GoToTop;
