84b1aafdcb2c2f04016b82dc10e6ce3bd3dfb7c3
Gabr4ca <jvgamingvn@gmail.com>
2025-10-17 20:29:09 +0700

    Update API URL handling to use environment variables for production and development

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login/Login";
 
 const App = () => {
-  const url = "https://uma.gabrys.io.vn/";
+  const url = import.meta.env.PROD ? "https://uma.gabrys.io.vn/" : "http://localhost:4000/";
   return (
     <div>
       <ToastContainer />
